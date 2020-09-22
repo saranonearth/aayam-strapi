0a13c5ee92fc3cf52ce618623b0c9644d5a221e7
saranonearth <saran.suresh@outlook.com>
2020-09-22 19:51:38 +0530

    Api db env updated

--- api/config/database.js
+++ api/config/database.js
@@ -1,20 +1,20 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'mongoose',
-      settings: {
-        host: env('DATABASE_HOST', 'aayam.oci2b.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'aayam'),
-        username: env('DATABASE_USERNAME', 'admin'),
-        password: env('DATABASE_PASSWORD', 'aayam123'),
-      },
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
-      },
+    defaultConnection: "default",
+    connections: {
+        default: {
+            connector: "mongoose",
+            settings: {
+                host: env("DATABASE_HOST", "aayam.oci2b.mongodb.net"),
+                srv: env.bool("DATABASE_SRV", true),
+                port: env.int("DATABASE_PORT", process.env.DATABASE_PORT),
+                database: env("DATABASE_NAME", process.env.DATABASE_NAME),
+                username: env("DATABASE_USERNAME", process.env.DATABASE_USERNAME),
+                password: env("DATABASE_PASSWORD", process.env.DATABASE_PASSWORD),
+            },
+            options: {
+                authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+                ssl: env.bool("DATABASE_SSL", true),
+            },
+        },
     },
-  },
 });
