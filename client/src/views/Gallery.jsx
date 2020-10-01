1844ac69e0bd778c7d5f01ddafa87a8ece3fa7d5
saranonearth <saran.suresh@outlook.com>
2020-10-01 19:27:03 +0530

    Connect gallery with api | Closes #5

--- client/src/views/Gallery.jsx
+++ client/src/views/Gallery.jsx
@@ -1,29 +1,30 @@
 import React, { useEffect, useState } from "react";
 import Logo from "../assets/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import _ from "../config";
 import Loader from "../components/Loader";
 import Empty from "../assets/empty.png";
-const Gallery = ({ props }) => {
-  console.log(props);
-
+const Gallery = () => {
   const [state, setState] = useState({
     data: null,
     loading: true,
   });
+
+  let params = useParams();
+  const id = params.id;
 
   useEffect(() => {
     window.scrollTo(0, 0);
     let isCancelled = false;
     const fetchData = async () => {
       try {
-        const response = await axios.get(`${_.API_URL}/galleries`);
+        const response = await axios.get(`${_.API_URL}/galleries/${id}`);
         console.log(response.data);
         if (!isCancelled) {
           setState({
             ...state,
-            data: response.data[0],
+            data: response.data,
             loading: false,
           });
         }
@@ -54,32 +55,43 @@ const Gallery = ({ props }) => {
         </div>
 
         <div className="content">
-          <p className="title-text">Gallery</p>
+          <p className="title-text">{state.data && state.data.FolderName}</p>
         </div>
         <div className="dash"></div>
       </div>
 
-      <div className="container">
-        <div className="gallery-container w-3 h-3">
-          <div className="gallery-item">
-            <div className="image">
-              <img
-                src="https://source.unsplash.com/1600x900/?nature"
-                alt="people"
-              />
+      {state.loading ? (
+        <div className="center mt-4">
+          <Loader />
+        </div>
+      ) : state.data ? (
+        <>
+          <div className="container">
+            {state.data.Images.map((d, i) => (
+              <div key={i} className="gallery-container w-3 h-3">
+                <div className="gallery-item">
+                  <div className="image">
+                    <img src={d.url} alt={d.caption} />
+                  </div>
+                  <div className="text">{d.caption}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      ) : (
+        <div className="center flex-it">
+          <div>
+            <div>
+              <img className="empty-icon" src={Empty} alt="empty-icon" />
             </div>
-            <div className="text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis,
-              ratione!
+
+            <div>
+              <p>No courses. Check again later </p>
             </div>
           </div>
         </div>
-      </div>
-      <div className="center">
-        <div className="button-dark">
-          <p>View More.</p>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
